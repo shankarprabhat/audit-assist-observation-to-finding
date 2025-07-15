c9a350a2136fb769d51a34d8753537b6957bccce
prabhat shankar <prabhat4686@gmail.com>
2025-07-15 08:03:54 +0530

    updated cloud embedding to true

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # --- GLOBAL CONFIGURATION FLAGS ---
 # Set to True to use HF Cloud Endpoint for embeddings; False for local FastEmbed
-USE_CLOUD_EMBEDDING = False 
+USE_CLOUD_EMBEDDING = True
 
 # Set to True to use ChromaDB as vector store; False for local disk persistence
 USE_CHROMA_DB = False
